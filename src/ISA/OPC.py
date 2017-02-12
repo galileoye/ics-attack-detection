11f510e868712d9cd8ccaed33f055274dfda0e31
Manikanta Reddy D <manikanta.reddy.d@live.com>
2017-02-12 19:14:09 +0530

    Done with ISA

--- src/ISA/OPC.py
+++ src/ISA/OPC.py
@@ -19,6 +19,8 @@ while 1:
     while time.time() - t < 1:
         continue
     t = time.time()
+
+    #Read registers from the specific zone
     l1 = opc1_client.read_holding_registers(L1, 1).registers[0]
     l2 = opc2_client.read_holding_registers(L2, 1).registers[0]
     t1 = opc1_client.read_holding_registers(T1, 1).registers[0]
@@ -29,4 +31,19 @@ while 1:
     f1 = opc2_client.read_holding_registers(F1, 1).registers[0]
     f2 = opc1_client.read_holding_registers(F2, 1).registers[0]
     f3 = opc2_client.read_holding_registers(F3, 1).registers[0]
-    h  = opc1_client.read_holding_registers(H, 1).registers[0]
+    h  = opc1_client.read_holding_registers( H, 1).registers[0]
+
+    #write to the register of the other zone
+    opc2_client.write_register(L1, l1)
+    opc1_client.write_register(L2, l2)
+    opc2_client.write_register(T1, t1)
+    opc1_client.write_register(T2, t2)
+    opc1_client.write_register(V1, v1)
+    opc2_client.write_register(V2, v2)
+    opc1_client.write_register( P,  p)
+    opc1_client.write_register(F1, f1)
+    opc2_client.write_register(F2, f2)
+    opc1_client.write_register(F3, f3)
+    opc2_client.write_register( H,  h)
+    
+    
